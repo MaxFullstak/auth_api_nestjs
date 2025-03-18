f29d9df9daa78f02222ab31a59fbbb07bce015d2
MaxFullstak <backmaxfront@gmail.com>
2025-03-18 20:46:55 +0300

    finish to code guards and implement some logic in UserController

--- src/auth/guards/auth.guard.ts
+++ src/auth/guards/auth.guard.ts
@@ -1,29 +1,29 @@
 import {
-	CanActivate,
-	ExecutionContext,
-	Injectable,
-	UnauthorizedException
-} from '@nestjs/common'
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from "@nestjs/common";
 
-import { UserService } from '@/user/user.service'
+import { UserService } from "@/user/user.service";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-	public constructor(private readonly userService: UserService) {}
+  public constructor(private readonly userService: UserService) {}
 
-	public async canActivate(context: ExecutionContext): Promise<boolean> {
-		const request = context.switchToHttp().getRequest()
+  public async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
 
-		if (typeof request.session.userId === 'undefined') {
-			throw new UnauthorizedException(
-				'Пользователь не авторизован. Пожалуйста, войдите в систему, чтобы получить доступ.'
-			)
-		}
+    if (typeof request.session.userId === "undefined") {
+      throw new UnauthorizedException(
+        "Пользователь не авторизован. Пожалуйста, войдите в систему, чтобы получить доступ."
+      );
+    }
 
-		const user = await this.userService.findById(request.session.userId)
+    const user = await this.userService.findById(request.session.userId);
 
-		request.user = user
+    request.user = user;
 
-		return true
-	}
+    return true;
+  }
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,24 @@
-import { Controller } from '@nestjs/common';
-import { UserService } from './user.service';
+import { Authorization } from "@/auth/decorators/auth.decorator";
+import { Authorized } from "@/auth/decorators/authorized.decorator";
+import { Controller, Get, HttpCode, HttpStatus, Param } from "@nestjs/common";
+import { UserService } from "./user.service";
+import { UserRole } from "@prisma/__generated__";
 
-@Controller('users')
+@Controller("users")
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Authorization()
+  @Get("profile")
+  @HttpCode(HttpStatus.OK)
+  public async findProfile(@Authorized("id") userId: string) {
+    return this.userService.findById(userId);
+  }
+
+  @Authorization(UserRole.ADMIN)
+	@HttpCode(HttpStatus.OK)
+	@Get('by-id/:id')
+	public async findById(@Param('id') id: string) {
+		return this.userService.findById(id)
+	}
 }
