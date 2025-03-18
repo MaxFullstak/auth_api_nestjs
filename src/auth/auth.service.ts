926a95296ef52eab65b10085610bdd3af7dcd85e
MaxFullstak <backmaxfront@gmail.com>
2025-03-18 20:08:54 +0300

    implement basic authentication(login, logout, saveSession) and connect to Redis database. Test the connect and start to code decorators with guards

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,20 +1,30 @@
 import { UserService } from "@/user/user.service";
-import { ConflictException, Injectable } from "@nestjs/common";
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+  UnauthorizedException,
+} from "@nestjs/common";
 import { AuthMethod, User } from "@prisma/__generated__";
 import { RegisterDto } from "./dto/register.dto";
+import { Request, Response } from "express";
+import { LoginDto } from "./dto/login.dto";
+import { verify } from "argon2";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class AuthService {
   public constructor(
     // private readonly prismaService: PrismaService,
-    private readonly userService: UserService
-    // private readonly configService: ConfigService,
+    private readonly userService: UserService,
+    private readonly configService: ConfigService,
     // private readonly providerService: ProviderService,
     // private readonly emailConfirmationService: EmailConfirmationService,
     // private readonly twoFactorAuthService: TwoFactorAuthService
   ) {}
 
-  public async register(dto: RegisterDto) {
+  public async register(req: Request, dto: RegisterDto) {
     const isExists = await this.userService.findByEmail(dto.email);
 
     if (isExists) {
@@ -39,14 +49,61 @@ export class AuthService {
     // 		'Вы успешно зарегистрировались. Пожалуйста, подтвердите ваш email. Сообщение было отправлено на ваш почтовый адрес.'
     // }
 
-    return this.saveSession(newUser);
+    return this.saveSession(req, newUser);
   }
 
-  public async login() {}
+  public async login(req: Request, dto: LoginDto) {
+    const user = await this.userService.findByEmail(dto.email);
 
-  public async logout() {}
+    if (!user || !user.password) {
+      throw new NotFoundException(
+        "Пользователь не найден. Пожалуйста, проверьте введенные данные"
+      );
+    }
 
-  private async saveSession(user: User) {
-    console.log("Session save. User: ", user);
+    const isValidPassword = await verify(user.password, dto.password);
+
+    if (!isValidPassword) {
+      throw new UnauthorizedException(
+        "Неверный пароль. Пожалуйста, попробуйте еще раз или восстановите пароль, если забыли его."
+      );
+    }
+
+    return this.saveSession(req, user);
+  }
+
+  public async logout(req: Request, res: Response): Promise<void> {
+    return new Promise((resolve, reject) => {
+      req.session.destroy((err) => {
+        if (err) {
+          return reject(
+            new InternalServerErrorException(
+              "Не удалось завершить сессию. Возможно, возникла проблема с сервером или сессия уже была завершена."
+            )
+          );
+        }
+        res.clearCookie(this.configService.getOrThrow<string>("SESSION_NAME"));
+        resolve();
+      });
+    });
+  }
+
+  private async saveSession(req: Request, user: User) {
+    return new Promise((resolve, reject) => {
+      req.session.userId = user.id;
+
+      req.session.save((err) => {
+        if (err) {
+          return reject(
+            new InternalServerErrorException(
+              "Не удалось сохранить сессию. Проверьте, правильно ли настроены параметры сессии."
+            )
+          );
+        }
+        resolve({
+          user,
+        });
+      });
+    });
   }
 }
